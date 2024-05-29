a7c11f9a726c97674647c1298177a823f2160fcc
JulioELS <u202011752@upc.edu.pe>
2024-05-29 17:07:25 -0500

    Update PagoRespository.java

--- src/main/java/pe/edu/upc/TFFinanzas/repositories/PagoRespository.java
+++ src/main/java/pe/edu/upc/TFFinanzas/repositories/PagoRespository.java
@@ -1,19 +1,12 @@
 package pe.edu.upc.TFFinanzas.repositories;
 
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import pe.edu.upc.TFFinanzas.entities.Pago;
 
-import java.util.List;
 
 @Repository
 public interface PagoRespository extends JpaRepository<Pago, Long>{
 
-    //BUSCAR PAGO POR ID
-    @Query("from pago pgo where pgo.idPago = :idPago")
-    List<Pago> buscarPago(@Param("idPago") Long idPago);
-    
 }
