e75f64793179b7714a0a53e735e3adc95453389d
JulioELS <u202011752@upc.edu.pe>
2024-05-29 16:23:49 -0500

    Update CreditoRepository.java

--- src/main/java/pe/edu/upc/TFFinanzas/repositories/CreditoRepository.java
+++ src/main/java/pe/edu/upc/TFFinanzas/repositories/CreditoRepository.java
@@ -1,12 +1,17 @@
 package pe.edu.upc.TFFinanzas.repositories;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import pe.edu.upc.TFFinanzas.entities.Credito;
 
+import java.util.List;
+
 @Repository
 public interface CreditoRepository extends JpaRepository<Credito, Long>{
 
-    
+    @Query("from credito crd where crd.idCredito = :idCredito")
+    List<Credito> buscarCredito(@Param("idCredito") Long idCredito);
 } 
